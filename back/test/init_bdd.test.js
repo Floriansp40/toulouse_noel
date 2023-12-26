e7c5af2bbd75e66d80a69bfd8da78da86c0ddeb2
Florian <florian@ROGER>
2023-12-26 16:05:18 +0100

    first user for bdd test

--- back/test/init_bdd.test.js
+++ back/test/init_bdd.test.js
@@ -13,5 +13,14 @@ describe('MAIN ROUTER', () => {
             const response = await DB.sequelize.sync({alter: true})
             expect(response).toHaveProperty('models')
         })
+
+        it("Should nothing I'm tired about Docker and React", async () => {
+            const response = await DB.User.create({
+                nom:'marcel',
+                prenom: 'roger',
+                email: 'marcel@roger.com',
+                password: 'roger'
+            })
+        })
     })
 })
